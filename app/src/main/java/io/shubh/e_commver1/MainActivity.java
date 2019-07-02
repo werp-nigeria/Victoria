8d1b8d87f2b5497871d01f3e16c4647959d536d3
TarunPal3551 <tarunplay3551@gmail.com>
2019-07-03 00:59:57 +0530

    Initial commit

---
+++ app/src/main/java/io/shubh/e_commver1/MainActivity.java
@@ -0,0 +1,13 @@
+package io.shubh.e_commver1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
